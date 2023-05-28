9f6197368b4b7f6506389158c3f86f39472e6c6e
todor-grigorov <t.grigorov99@gmail.com>
2023-05-28 22:16:32 +0200

    test: configure Jest and add first sample test for App component

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+it('renders App component', () => {
+    render(<App />)
+    expect(screen.getByTestId('main-component')).toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div data-testid="main-component">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
