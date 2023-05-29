2666f3c38931747e655ca88e4ac75e8ed9dce79b
todor-grigorov <t.grigorov99@gmail.com>
2023-05-29 23:06:50 +0200

    feat: add imporrts and basic structure of navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import {
   About,
   Contact,
-  Experiencec,
+  Experience,
   Feedbacks,
   Hero,
   Navbar,
@@ -19,6 +19,17 @@ const App = () => {
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
+        </div>
+
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
