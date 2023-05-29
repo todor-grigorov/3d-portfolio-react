5fd36adca42d98abfb60e7d68e162309565a8930
todor-grigorov <t.grigorov99@gmail.com>
2023-05-29 22:42:37 +0200

    feat: add init navigation to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import {
+  About,
+  Contact,
+  Experiencec,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from './components'
+
 const App = () => {
-  return <div>3D Developer Portfolio</div>
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App
