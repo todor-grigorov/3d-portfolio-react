994f68dbea0aecd3d11bb775f110f3441ba55f92
todor-grigorov <t.grigorov99@gmail.com>
2023-05-31 23:09:27 +0200

    feat: display navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,27 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18ox] font-bold cursor-pointer">
+            Todor Grigorov
+            <span className="sm:block hidden">Software Developer</span>
+          </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? 'text-white' : 'text-secondary'
+              } 
+              hover:text-white text-[18px] 
+              font-medium cursor-pointer
+              `}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
