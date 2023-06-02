b9cc20c1482dcaf8b9d8868a82b2d458661469b3
todor-grigorov <t.grigorov99@gmail.com>
2023-06-02 23:59:37 +0200

    feat: add ComputersCanvas component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,6 +2,8 @@ import React, { Suspense, useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
+import CanvasLoader from '../Loader'
+
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
@@ -13,4 +15,26 @@ const Computers = () => {
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas
