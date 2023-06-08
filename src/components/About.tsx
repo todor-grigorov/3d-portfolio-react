275972ba037c9841e5619ae03a10ba65a4e45537
todor-grigorov <t.grigorov99@gmail.com>
2023-06-08 14:43:09 +0200

    feat: add ServiceCard component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
+import { Direction } from '../Types/Direction'
 
 interface IServiceProps {
   title: string
@@ -12,7 +13,29 @@ interface IServiceProps {
 }
 
 const ServiceCard = ({ title, icon, index }: IServiceProps) => {
-  return <Tilt></Tilt>
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn(Direction.RIGHT, 'spring', 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          // options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img
+            src={icon}
+            alt="web-development"
+            className="w-16 h-16 object-contain"
+          />
+
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
 }
 
 const About = () => {
