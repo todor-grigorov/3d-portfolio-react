8fbb9c265b54e893d15dcaae8dd8eaf87319ca0e
todor-grigorov <t.grigorov99@gmail.com>
2023-06-08 15:03:40 +0200

    feat: finish SectionWrapper

--- src/hoc/SectionWrapper.tsx
+++ src/hoc/SectionWrapper.tsx
@@ -5,12 +5,28 @@ import { styles } from '../styles'
 import { staggerContainer } from '../utils/motion'
 
 interface ISectionWrapperProps {
-  Component: React.ReactNode
+  Component: React.FC
   idName: string
 }
 
 const SectionWrapper = ({ Component, idName }: ISectionWrapperProps) => {
-  return <div>SectionWrapper</div>
+  function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        <span className="hash-span" id={idName}>
+          &nbsp;
+        </span>
+
+        <Component />
+      </motion.section>
+    )
+  }
 }
 
 export default SectionWrapper
