556f93f60a416554e3e2b442b62cbe96c33c9f9e
todor-grigorov <t.grigorov99@gmail.com>
2023-06-08 14:54:17 +0200

    feat: add imports, props and interfaces

--- src/hoc/SectionWrapper.tsx
+++ src/hoc/SectionWrapper.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
-const SectionWrapper = () => {
+import { styles } from '../styles'
+import { staggerContainer } from '../utils/motion'
+
+interface ISectionWrapperProps {
+  Component: React.ReactNode
+  idName: string
+}
+
+const SectionWrapper = ({ Component, idName }: ISectionWrapperProps) => {
   return <div>SectionWrapper</div>
 }
 
