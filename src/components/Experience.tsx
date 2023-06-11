a9a0c849db0e15203013a0950c5db5a688e72d84
todor-grigorov <t.grigorov99@gmail.com>
2023-06-11 21:11:36 +0200

    feat: add experienceCard component v0.1

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-// import {
-//   VerticalTimeline,
-//   VerticalTimelineElement,
-// } from 'react-vertical-timeline-component'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component'
 import { motion } from 'framer-motion'
 import { SectionWrapper } from '../hoc'
 
@@ -12,6 +12,50 @@ import { styles } from '../styles'
 // import { experiences } from '../constants'
 // import { SectionWrapper } from '../hoc'
 import { textVariant } from '../utils/motion'
+
+const ExperienceCard = ({ experience }) => {
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        background: '#1d1836',
+        color: '#fff',
+      }}
+      contentArrowStyle={{ borderRight: '7px solid  #232631' }}
+      date={experience.date}
+      iconStyle={{ background: experience.iconBg }}
+      icon={
+        <div className="flex justify-center items-center w-full h-full">
+          <img
+            src={experience.icon}
+            alt={experience.company_name}
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+      }
+    >
+      <div>
+        <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+        <p
+          className="text-secondary text-[16px] font-semibold"
+          style={{ margin: 0 }}
+        >
+          {experience.company_name}
+        </p>
+      </div>
+
+      <ul className="mt-5 list-disc ml-5 space-y-2">
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className="text-white-100 text-[14px] pl-1 tracking-wider"
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  )
+}
 
 const Experience = () => {
   return (
@@ -24,6 +68,10 @@ const Experience = () => {
           Work Experience.
         </h2>
       </motion.div>
+
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline></VerticalTimeline>
+      </div>
     </>
   )
 }
