27b84590efae63ba084824f216881b3be85e1130
todor-grigorov <t.grigorov99@gmail.com>
2023-06-13 22:42:27 +0200

    feat: add BallCanvas component

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -41,4 +41,21 @@ const Ball = ({ imgUrl }: BallProps) => {
   )
 }
 
-export default Ball
+const BallCanvas = ({ icon }: { icon: string }) => {
+  return (
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default BallCanvas
