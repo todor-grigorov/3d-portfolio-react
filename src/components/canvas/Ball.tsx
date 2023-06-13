6bd0ef42efd4095f8e7c5fe0a9c949a6fbde34e8
todor-grigorov <t.grigorov99@gmail.com>
2023-06-13 22:41:14 +0200

    feat: add jsx + interfce

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,15 +1,44 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
 import {
   Decal,
   Float,
   OrbitControls,
   Preload,
   useTexture,
-} from "@react-three/drei";
+} from '@react-three/drei'
 
-const Ball = () => {
-  return <div>Ball</div>
+import CanvasLoader from '../Loader'
+
+interface BallProps {
+  imgUrl: string
+}
+
+const Ball = ({ imgUrl }: BallProps) => {
+  const [decal] = useTexture([imgUrl])
+
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  )
 }
 
 export default Ball
