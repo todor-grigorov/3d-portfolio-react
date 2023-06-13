42a2646f4caaf53903dc17d522281fe4bf6c29d5
todor-grigorov <t.grigorov99@gmail.com>
2023-06-13 22:39:56 +0200

    feat: add imports to Ball component

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,3 +1,13 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+
 const Ball = () => {
   return <div>Ball</div>
 }
