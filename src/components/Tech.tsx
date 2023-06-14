6499b662a821cc0f81b5592c3eeeb3caa401366d
todor-grigorov <t.grigorov99@gmail.com>
2023-06-14 21:26:24 +0200

    feat: add imports to tech comopnent

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,3 +1,9 @@
+import React from 'react'
+
+import { BallCanvas } from './canvas'
+import { SectionWrapper } from '../hoc'
+import { technologies } from '../constants'
+
 const Tech = () => {
   return <div>Tech</div>
 }
