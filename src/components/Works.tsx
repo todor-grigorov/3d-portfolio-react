3964a5e6e467c315841f1667ac38c31d7cba7089
todor-grigorov <t.grigorov99@gmail.com>
2023-06-17 20:44:24 +0200

    feat: add init project card component

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -7,6 +7,22 @@ import { github } from '../assets'
 import { SectionWrapper } from '../hoc'
 import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
+import { Direction } from '../Types/Direction'
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div
+      variants={fadeIn(Direction.UP, 'spring', index * 0.5, 0.75)}
+    ></motion.div>
+  )
+}
 
 const Works = () => {
   return <div>Works</div>
