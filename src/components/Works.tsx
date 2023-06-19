6b428b5942dffe75b624f37ab0cfbc96a2fa8219
todor-grigorov <t.grigorov99@gmail.com>
2023-06-19 22:47:31 +0200

    refactor: component ProjectCard to use new types

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -8,28 +8,63 @@ import { SectionWrapper } from '../hoc'
 import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 import { Direction } from '../Types/Direction'
+import { Project } from '../Types/Project'
 
 interface IProjectCardProps {
   index: number
-  name: string
-  description: string
-  tags: string[]
-  image: string
-  source_code_link: string
+  project: Project
 }
 
-const ProjectCard = ({
-  index,
-  name,
-  description,
-  tags,
-  image,
-  source_code_link,
-}: IProjectCardProps) => {
+const ProjectCard = ({ index, project }: IProjectCardProps) => {
+  const { name, description, tags, image, source_code_link } = project
   return (
-    <motion.div
-      variants={fadeIn(Direction.UP, 'spring', index * 0.5, 0.75)}
-    ></motion.div>
+    <motion.div variants={fadeIn(Direction.UP, 'spring', index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, '_blank')}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
   )
 }
 
