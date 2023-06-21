e61d910ed435cdb942135ca64fcd8eec8f702124
todor-grigorov <t.grigorov99@gmail.com>
2023-06-21 22:20:18 +0200

    feat: complete Works component UI

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -70,15 +70,15 @@ const ProjectCard = ({ index, project }: IProjectCardProps) => {
 const Works = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
+      <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
-      <div className='w-full flex'>
+      <div className="w-full flex">
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          variants={fadeIn('', '', 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
@@ -87,7 +87,17 @@ const Works = () => {
           and manage projects effectively.
         </motion.p>
       </div>
+
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            project={project}
+          />
+        ))}
+      </div>
     </>
   )
-
+}
 export default Works
