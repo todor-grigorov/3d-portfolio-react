659f2d6ab9269a6525a9701bbba1ada076efb2f7
todor-grigorov <t.grigorov99@gmail.com>
2023-06-23 23:01:41 +0200

    fix: expor tof works component

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,73 +1,13 @@
 import React from 'react'
-import Tilt from 'react-parallax-tilt'
 import { motion } from 'framer-motion'
 
 import { styles } from '../styles'
-import { github } from '../assets'
 import { SectionWrapper } from '../hoc'
 import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
-import { Direction } from '../Types/Direction'
-import { Project } from '../Types/Project'
+import ProjectCard from './ProjectCard'
 
-interface IProjectCardProps {
-  index: number
-  project: Project
-}
-
-const ProjectCard = ({ index, project }: IProjectCardProps) => {
-  const { name, description, tags, image, source_code_link } = project
-  return (
-    <motion.div variants={fadeIn(Direction.UP, 'spring', index * 0.5, 0.75)}>
-      <Tilt
-        tiltMaxAngleX={20}
-        tiltMaxAngleY={35}
-        scale={1}
-        transitionSpeed={450}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-      >
-        <div className="relative w-full h-[230px]">
-          <img
-            src={image}
-            alt="project_image"
-            className="w-full h-full object-cover rounded-2xl"
-          />
-
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-            <div
-              onClick={() => window.open(source_code_link, '_blank')}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img
-                src={github}
-                alt="source code"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
-
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
-        </div>
-      </Tilt>
-    </motion.div>
-  )
-}
-
-const Works = () => {
+const Works: React.FC = (): JSX.Element => {
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -100,4 +40,4 @@ const Works = () => {
     </>
   )
 }
-export default Works
+export default SectionWrapper(Works, '')
