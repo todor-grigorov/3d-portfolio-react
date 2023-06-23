17fc0f563d7c43c3449997b2e0c1205c0b09ddd1
todor-grigorov <t.grigorov99@gmail.com>
2023-06-23 23:01:13 +0200

    refactor: extract project card component in seperate file

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,68 @@
+import Tilt from 'react-parallax-tilt'
+import { motion } from 'framer-motion'
+import { github } from '../assets'
+import { Direction } from '../Types/Direction'
+import { Project } from '../Types/Project'
+import { fadeIn } from '../utils/motion'
+
+interface IProjectCardProps {
+  index: number
+  project: Project
+}
+
+const ProjectCard: React.FC<IProjectCardProps> = ({
+  index,
+  project,
+}: IProjectCardProps): JSX.Element => {
+  const { name, description, tags, image, source_code_link } = project
+  return (
+    <motion.div variants={fadeIn(Direction.UP, 'spring', index * 0.5, 0.75)}>
+      <Tilt
+        tiltMaxAngleX={20}
+        tiltMaxAngleY={35}
+        scale={1}
+        transitionSpeed={450}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, '_blank')}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  )
+}
+
+export default ProjectCard
